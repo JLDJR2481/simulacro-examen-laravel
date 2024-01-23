5562fc9a3f0e3a7fbd7700c6cf7c0562ac038c78
Jose/Xulo/Chelu <jjesusramon@cifpfbmoll.eu>
2024-01-23 17:42:13 +0100

    feat(): actividad 3, old values

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -46,29 +46,32 @@
             <div class="form-group">
                 <label for="title" class="h4">Título de la publicación</label><br>
                 <input type="text" name="title" class="form-control-lg mb-3" required
-                    placeholder="Ingresa aquí el título de la publicación">
+                    placeholder="Ingresa aquí el título de la publicación" value="{{ old('titulo') }}">
             </div>
             <div class="form-group">
                 <label for="extracto" class="h4">Extracto publicación</label><br>
                 <textarea rows="3" name="extracto" class="form-control-lg w-100 mb-3" required
-                    placeholder="Ingresa un extracto de la publicación"></textarea>
+                    placeholder="Ingresa un extracto de la publicación" value="{{ old('extracto') }}"></textarea>
             </div>
             <div class="form-group">
                 <label for="content" class="h4">Contenido publicación</label><br>
                 <textarea rows="5" name="content" class="form-control-lg w-100 mb-3" required
-                    placeholder="Ingresa el contenido de la publicación"></textarea>
+                    placeholder="Ingresa el contenido de la publicación" value="{{ old('content') }}"></textarea>
             </div>
             <div class="container-fluid">
                 <div class="container">
                     <div class="form-check">
                         <label for="caducable" class="form-check-label h4">¿Caducable?</label>
-                        <input type="checkbox" name="caducable" id="caducable_true" class="form-check-input">
+                        <input type="checkbox" name="caducable[]" id="caducable_true" class="form-check-input" value="1"
+                            @if (is_array(old('caducable')) && in_array(1, old('caducable'))) checked @endif>
                     </div>
                 </div>
                 <div class="container">
                     <div class="form-check">
                         <label for="comentable" class="form-check-label h4">¿Comentable?</label>
-                        <input type="checkbox" name="comentable" id="comentable_true" class="form-check-input">
+                        <input type="checkbox" name="comentable[]" id="comentable_true" class="form-check-input"
+                            value="1" @if (is_array(old('comentable')) && in_array(1, old('comentable'))) checked
+                            @endif>
                     </div>
                 </div>
             </div>
@@ -76,8 +79,8 @@
             <div class="form-group">
                 <label for="acceso" class="form-control"> Acceso:</label>
                 <select name="acceso" class="form-control">
-                    <option value="privado">Privado</option>
-                    <option value="publico">Público</option>
+                    <option value="privado" @if (old('acceso')=='privado' ) selected @endif>Privado</option>
+                    <option value="publico" @if (old('acceso')=='publico' ) selected @endif>Público</option>
                 </select>
             </div>
 
