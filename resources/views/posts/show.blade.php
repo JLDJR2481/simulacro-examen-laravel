810a196cb01a11cd0562e1300116ccee21cc8901
jjesusramon@cifpfbmoll.eu <jjesusramon@cifpfbmoll.eu>
2024-01-27 23:42:08 +0100

    feat(): Realizada vista de visualización posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,20 @@
 <x-app-layout>
     <div class="container mt-5">
-        <div class="row justify-content-center"></div>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class='card'>
+                    <div class="card-body">
+                        <h1 class="card-title">@lang("formulario.title"): {{$post->titulo}}</h1>
+                        <p class="card-text">@lang("formulario.extract"): {{$post->extracto}}</p>
+                        <p class="card-text">@lang("formulario.content"): {{$post->contenido}}</p>
+                        <p class="card-text">@lang("formulario.expiring"): {{$post->caducable ? __('Yes') : __('No')}}
+                        </p>
+                        <p class="card-text">@lang("formulario.commentable"): {{$post->comentable ? __('Yes') :
+                            __('No')}}</p>
+                        <p class="card-title">@lang("formulario.access"): {{$post->acceso}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </x-app-layout>
